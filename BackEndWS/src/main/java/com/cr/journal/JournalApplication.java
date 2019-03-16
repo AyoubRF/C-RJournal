a259e776432f8f42dc0cb139678e5231380014ce
AyoubRF <ayoubraffass@gmail.com>
2019-03-17 00:25:55 +0100

    JWT Authentification Configuration

--- BackEndWS/src/main/java/com/cr/journal/JournalApplication.java
+++ BackEndWS/src/main/java/com/cr/journal/JournalApplication.java
@@ -2,6 +2,9 @@ package com.cr.journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class JournalApplication {
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
