88ff9a2bf40c1f1882bda3634c81e3cf554056d7
mohammedrabii <mohammedrabii_1995@hotmail.com>
2019-03-18 08:48:33 +0100

    modified jpa repository

--- BackEndWS/src/main/java/com/cr/journal/dao/repository/UserRepository.java
+++ BackEndWS/src/main/java/com/cr/journal/dao/repository/UserRepository.java
@@ -4,7 +4,4 @@ package com.cr.journal.dao.repository;
 import com.cr.journal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer >
-{
-
-}
+public interface UserRepository extends JpaRepository<User,Integer > {}
