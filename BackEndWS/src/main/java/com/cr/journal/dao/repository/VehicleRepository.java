1fc52220c1f0377c67da51ace77f76a4bb901e67
Ayoub <ayoubraffass@gmail.com>
2019-03-19 14:02:27 +0100

    Code Cleanup

--- BackEndWS/src/main/java/com/cr/journal/dao/repository/VehicleRepository.java
+++ BackEndWS/src/main/java/com/cr/journal/dao/repository/VehicleRepository.java
@@ -1,10 +1,12 @@
 package com.cr.journal.dao.repository;
 
 import com.cr.journal.entity.Vehicle;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //Exposing Vehicle API via Repository interface directly (Spring Data Rest)
-@RepositoryRestResource(path = "vehicle", collectionResourceRel = "vehicle", itemResourceRel = "vehicle")//NOT WORKING YET
+//@RepositoryRestResource(path = "vehicle", collectionResourceRel = "vehicle", itemResourceRel = "vehicle")//NOT WORKING YET
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
