a3e7695f05e19976ace0452dc89761ef0635bda3
Ayoub <ayoubraffass@gmail.com>
2019-04-18 19:19:12 +0100

    Fix Front Reactive Add User

--- FrontEndWS/src/app/admin/store/reducer/admin.reducer.ts
+++ FrontEndWS/src/app/admin/store/reducer/admin.reducer.ts
@@ -14,6 +14,7 @@ export function reducer(state = intialUserState, action: ActionAdminActions): IU
       return{
         ...state,
         selectedUser:action.payload,
+        users : [...state.users,action.payload]
       }
     }
     default:
